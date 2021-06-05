93ee2d20828ca1af24046b8e7bec418db15aa53b
costcon <tentekomambo@gmail.com>
2021-06-05 22:39:41 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,39 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get 'review/index'
-  get 'review/show'
-  get 'review/create'
-  get 'review/update'
-  get 'reserves/new'
-  get 'reserves/confirm'
-  get 'reserves/create'
-  get 'reserves/index'
-  get 'reserves/show'
+
+  resources :reviews, only: [:index, :show, :create, :update]
+  resources :reserves, only: [:new, :create, :index, :show, ] do
+    collection do
+      post 'confirm'
+    end
+  end
+
+  resources :bikes, only: [:edit, :create, :index, :show, :update, :destroy]
+  resources :users, only: [:edit, :show, :update, :destroy]
+  
+  # get 'review/index'
+  # get 'review/show'
+  # get 'review/create'
+  # get 'review/update'
+  # get 'reserves/new'
+  # get 'reserves/confirm'
+  # get 'reserves/create'
+  # get 'reserves/index'
+  # get 'reserves/show'
+
   get 'reserves/thanks'
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bikes/edit'
-  get 'bikes/update'
-  get 'bikes/destroy'
-  get 'users/edit'
-  get 'users/destroy'
-  get 'users/show'
-  get 'users/update'
-  get 'homes/top'
-  get 'homes/about'
+
+  # get 'bikes/index'
+  # get 'bikes/show'
+  # get 'bikes/edit'
+  # get 'bikes/update'
+  # get 'bikes/destroy'
+  # get 'users/edit'
+  # get 'users/destroy'
+  # get 'users/show'
+  # get 'users/update'
+  # get 'homes/top'
+  # get 'homes/about'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
