5319e5d43e36b436820377aa4b56fa148e756736
costcon <tentekomambo@gmail.com>
2021-07-01 08:08:08 +0000

    Fix admin users show changestatus

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,5 @@
 class Admin::UsersController < ApplicationController
 
-  def edit
-  end
 
   def index
     @users = User.all.page(params[:page]).per(20)
@@ -22,7 +20,7 @@ class Admin::UsersController < ApplicationController
       redirect_to request.referer, success: "更新しました"
     else
       flash.now[:danger] = '作成に失敗しました。'
-      render :edit
+      render :show
     end
   end
 
@@ -30,7 +28,25 @@ class Admin::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :profile_image, :last_name, :last_name_kana, :first_name, :first_name_kana, :postal_code, :address, :telephone_number, :profile_comment, :email, :license_number, :license_date, :license_expiration, :license_front_image, :license_back_image, :approval, :id_deleted)
+    params.require(:user).permit(
+      :nickname,
+      :profile_image,
+      :last_name,
+      :last_name_kana,
+      :first_name,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :profile_comment,
+      :email,
+      :license_number,
+      :license_date,
+      :license_expiration,
+      :license_front_image,
+      :license_back_image,
+      :approval,
+      :id_deleted)
   end
 
 end
